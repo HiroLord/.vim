d401d316efa01c7f2f317d0749249dd09ffce568
Preston <prestonturner42@gmail.com>
2015-07-19 19:34:11 +0000

    Updated shell script to remove old symlinks.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
-sudo ln -s ~/.vim/vimrc ~/.vimrc
-sudo ln -s ~/.vim/tmux.conf ~/.tmux.conf
+read -p "This will overrite files if they exist. Are you sure you want to continue? (y/n) " reply
+case "$reply" in 
+	y|Y ) 
+	sudo rm ~/.vimrc
+	sudo rm ~/.tmux.conf
+	sudo ln -s ~/.vim/vimrc ~/.vimrc
+	sudo ln -s ~/.vim/tmux.conf ~/.tmux.conf
+	echo "Finished setup";;
+	n|N ) echo "Setup not completed.";;
+	* ) echo "Invalid option.";;
+esac
