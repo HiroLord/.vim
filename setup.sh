8438b56aa352b8301c7765ffcfa93ea95e8b46d0
Preston <prestonturner42@gmail.com>
2015-07-20 03:42:45 +0000

    Removed 'sudo' from setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 read -p "This will overrite files if they exist. Are you sure you want to continue? (y/n) " reply
 case "$reply" in 
 	y|Y ) 
-	sudo rm ~/.vimrc
-	sudo rm ~/.tmux.conf
-	sudo ln -s ~/.vim/vimrc ~/.vimrc
-	sudo ln -s ~/.vim/tmux.conf ~/.tmux.conf
+	rm ~/.vimrc
+	rm ~/.tmux.conf
+	ln -s ~/.vim/vimrc ~/.vimrc
+	ln -s ~/.vim/tmux.conf ~/.tmux.conf
 	echo "Finished setup";;
 	n|N ) echo "Setup not completed.";;
 	* ) echo "Invalid option.";;
