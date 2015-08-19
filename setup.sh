bacd19e5b9a2bb544eb61322a56409517d16f3ca
Preston Turner <prestonturner42@gmail.com>
2015-08-19 17:53:37 +0000

    Added inputrc to config

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
+echo "Editing directory" ~/;
 read -p "This will overrite files if they exist. Are you sure you want to continue? (y/n) " reply
 case "$reply" in 
-	y|Y ) 
+	y|Y )
 	rm ~/.vimrc
 	rm ~/.tmux.conf
+    rm ~/.inputrc
 	ln -s ~/.vim/vimrc ~/.vimrc
 	ln -s ~/.vim/tmux.conf ~/.tmux.conf
+    ln -s ~/.vim/inputrc ~/.inputrc
 	echo "Finished setup";;
 	n|N ) echo "Setup not completed.";;
 	* ) echo "Invalid option.";;
